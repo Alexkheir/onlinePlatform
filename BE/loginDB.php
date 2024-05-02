5ba424cf7e20792a2ce7f1382f2d37798ad54748
Your Name <you@example.com>
2024-05-02 21:17:03 +0300

    new hey

---
+++ BE/loginDB.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once("db.php");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $un = $_POST["username"]; 
+    $pass = $_POST["password"]; 
+
+    try {
+        $sql = "SELECT * FROM users WHERE username = '$un'";
+        $result = $pdo->query($sql);
+
+        if ($result->rowCount() > 0) {
+            $user = $result->fetch(PDO::FETCH_ASSOC);
+            if (password_verify($pass, $user['password'])) {
+                echo "Login successful!";
+            } else {
+                echo "Error: Incorrect password!";
+            }
+        } else {
+            echo "Error: User not found!";
+        }
+    } catch (PDOException $e) {
+        die("Error: " . $e->getMessage());
+    }
+}
+?>
